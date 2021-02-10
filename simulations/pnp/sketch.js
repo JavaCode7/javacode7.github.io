b141c8c1f9c041624134daac48985d6bdf791a94
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-10 12:08:51 +0000

    Finish Predator and prey

--- simulations/pnp/sketch.js
+++ simulations/pnp/sketch.js
@@ -1,36 +1,32 @@
-function make2DArray(cols, rows) {
-    let arr = new Array(cols);
+function make2DArray(c, r) {
+    let arr = new Array(c);
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
+        arr[i] = new Array(r);
     }
     return arr;
 }
 
 let grid;
-let energy;
 let cols;
 let rows;
 let gens = 1;
 let resolution = 4;
 
 function setup() {
-    createCanvas(900, 900);
+    createCanvas(displayWidth, displayHeight);
     cols = width / resolution;
     rows = height / resolution;
 
     grid = make2DArray(cols, rows);
-    energy = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             const r = floor(random(3));
-            const e = floor(random(10));
             if (r == 2) {
                 var a = 5;
             } else {
                 a = r;
             }
             grid[i][j] = a;
-            energy[i][j] = e;
         }
     }
 }
@@ -62,32 +58,20 @@ function draw() {
             let neighbors = countNeighbors(grid, i, j);
             let py = neighbors[1];
             let pd = neighbors[0];
-            if (state == 0 && py == 1) {
-                if (floor(random(100)) == 0) {
-                    next[i][j] = 1;
-                } else {
-                    next[i][j] = state;
-                }
-            } else if (state == 0 && pd == 1) {
-                if (floor(random(100)) == 0) {
+            if (state == 5 && pd) {
+                next[i][j] = 0;
+            } else if (state == 1 && pd) {
+                next[i][j] = 0;
+            } else if (state == 5 && !py) {
+                next[i][j] = 0;
+            } else if (state == 0 && py == 3) {
+                next[i][j] = 1;
+            } else if (state == 0 && (pd == 2)) {
+                if (floor(random(pd)) > 0) {
                     next[i][j] = 5;
-                } else {
-                    next[i][j] = state;
                 }
             } else {
-                if (floor(random(4)) == 0) {
-                    next[i][j] = 0;
-                    next[i + 1][j] = 5;
-                } else if (floor(random(4)) == 1) {
-                    next[i][j] = 0;
-                    next[i - 1][j] = 5;
-                } else if (floor(random(4)) == 2) {
-                    next[i][j] = 0;
-                    next[i][j + 1] = 5;
-                } else if (floor(random(4)) == 3) {
-                    next[i][j] = 0;
-                    next[i][j - 1] = 5;
-                }
+                next[i][j] = state;
             }
         }
     }
@@ -96,7 +80,7 @@ function draw() {
     grid = next;
 }
 
-function countNeighbors(grid, x, y) {
+function countNeighbors(g, x, y) {
     let preds = 0;
     let preys = 0;
     for (let i = -1; i < 2; i++) {
@@ -105,14 +89,14 @@ function countNeighbors(grid, x, y) {
             var col = (x + i + cols) % cols;
             var row = (y + j + rows) % rows;
 
-            if (grid[col][row] == 5) {
+            if (g[col][row] == 5) {
                 preds += 1;
             } else {
-                preys += grid[col][row]
+                preys += g[col][row]
             }
         }
     }
-    preds -= grid[x][y];
-    preys -= grid[x][y];
+    preds -= g[x][y];
+    preys -= g[x][y];
     return [preds, preys];
 }
